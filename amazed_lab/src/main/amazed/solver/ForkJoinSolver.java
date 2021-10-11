8788ad08e6683b15464900382f9c02c50366a0b4
sluggerbit <70601563+sluggerbit@users.noreply.github.com>
2021-10-11 16:30:55 +0200

    började lite
    Co-authored-by: nikaxe <nikaxe@users.noreply.github.com>

--- amazed_lab/src/main/amazed/solver/ForkJoinSolver.java
+++ amazed_lab/src/main/amazed/solver/ForkJoinSolver.java
@@ -4,10 +4,15 @@ import amazed.maze.Maze;
 
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Deque;
 import java.util.Map;
 import java.util.HashMap;
 import java.util.Set;
 import java.util.concurrent.ConcurrentSkipListSet;
+import java.util.concurrent.ConcurrentLinkedDeque;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.ForkJoinPool.ForkJoinWorkerThreadFactory;;
+
 
 /**
  * <code>ForkJoinSolver</code> implements a solver for
@@ -31,6 +36,8 @@ public class ForkJoinSolver
     public ForkJoinSolver(Maze maze)
     {
         super(maze);
+        visited = new ConcurrentSkipListSet<>();
+        cFrontier = new ConcurrentLinkedDeque<>();
     }
 
     /**
@@ -48,7 +55,11 @@ public class ForkJoinSolver
     {
         this(maze);
         this.forkAfter = forkAfter;
+        
     }
+
+    Deque<Integer> cFrontier;
+    int count = 0;
 
     /**
      * Searches for and returns the path, as a list of node
@@ -66,9 +77,53 @@ public class ForkJoinSolver
     {
         return parallelSearch();
     }
-
+    
     private List<Integer> parallelSearch()
-    {
+    { 
+        if(!visited.contains(start)){ 
+            cFrontier.push(start);
+            // new player when starting new fork
+            int player = maze.newPlayer(start);
+        }
+  
+        while(!cFrontier.isEmpty()){
+            int current = cFrontier.pop();    
+            
+            if(maze.hasGoal(current)){
+                maze.move(player, current);
+                return pathFromTo(start, current);
+            }
+
+            if(!visited.contains(current)){
+                maze.move(player, current);
+                visited.add(current);
+                List<ForkJoinSolver> forks = new ArrayList<>();
+                count += 1;
+                for (int nb: maze.neighbors(current)) {
+                    // add nb to the nodes to be processed
+                    cFrontier.push(nb);
+                    // if nb has not been already visited,
+                    // nb can be reached from current (i.e., current is nb's predecessor)
+                    if (!visited.contains(nb)){                      
+                        predecessor.put(nb, current);
+                        if(count % 9 == 0){
+                            
+                            ForkJoinSolver fork = new ForkJoinSolver(maze);
+                            forks.add(fork);
+                            fork.fork(); 
+                        }
+                    }
+                }
+                for (ForkJoinSolver fork : forks){
+                    List<Integer> path = fork.join();
+                    if(path != null)
+                        return path;
+                }
+                
+            }
+            
+        }
+
         return null;
     }
 }
